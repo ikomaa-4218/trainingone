f78e7e2281cf3ca91064e1119cbff4001bdf47af
azibfikri14 <azibfikri14@gmail.com>
2026-03-17 05:39:16 +0000

    Firebase setup

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase
 
 // TODO: Add your own Firebase configuration below
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyA8ZIgLFC13U41TLdxtMDRggIcgEocvjqI",
+  authDomain: "upskill-project-5b6d0.firebaseapp.com",
+  projectId: "upskill-project-5b6d0",
+  storageBucket: "upskill-project-5b6d0.firebasestorage.app",
+  messagingSenderId: "255889760637",
+  appId: "1:255889760637:web:bdf85e206048d18b8cac14",
+  measurementId: "G-BEG6WC3DKJ"
 };
 
 // Initialize Firebase
