f65d007f84877ba8b3f10f88d7243aed8e584f59
azibfikri14 <azibfikri14@gmail.com>
2026-03-17 07:43:08 +0000

    try again

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-// Make sure to import getAuth
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -17,8 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize and get a reference to the services
 const db = getFirestore(app);
 const auth = getAuth(app);
 
