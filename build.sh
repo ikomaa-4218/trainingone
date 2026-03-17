353d069fbb60a30dde8c2ba625d81680677f18ef
azibfikri14 <azibfikri14@gmail.com>
2026-03-17 08:06:04 +0000

    new fix

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 
 // Your web app's Firebase configuration, populated by environment variables
 const firebaseConfig = {
-  apiKey: "\$FIREBASE_API_KEY",
-  authDomain: "\$FIREBASE_AUTH_DOMAIN",
-  projectId: "\$FIREBASE_PROJECT_ID",
-  storageBucket: "\$FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "\$FIREBASE_MESSAGING_SENDER_ID",
-  appId: "\$FIREBASE_APP_ID",
-  measurementId: "\$FIREBASE_MEASUREMENT_ID"
+  apiKey: "$FIREBASE_API_KEY",
+  authDomain: "$FIREBASE_AUTH_DOMAIN",
+  projectId: "$FIREBASE_PROJECT_ID",
+  storageBucket: "$FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "$FIREBASE_MESSAGING_SENDER_ID",
+  appId: "$FIREBASE_APP_ID",
+  measurementId: "$FIREBASE_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
