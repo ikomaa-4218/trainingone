2b1b19ccc7b39359000bc5c3f2f11c0451a92e02
azibfikri14 <azibfikri14@gmail.com>
2026-03-17 07:31:17 +0000

    Change config

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyA8ZIgLFC13U41TLdxtMDRggIcgEocvjqI",
+  authDomain: "upskill-project-5b6d0.firebaseapp.com",
+  projectId: "upskill-project-5b6d0",
+  storageBucket: "upskill-project-5b6d0.appspot.com",
+  messagingSenderId: "255889760637",
+  appId: "1:255889760637:web:bdf85e206048d18b8cac14",
+  measurementId: "G-BEG6WC3DKJ"
 };
 
 // Initialize Firebase
